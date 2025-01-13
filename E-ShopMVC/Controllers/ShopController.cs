2b490384ea457d616bad11dfd7aefd05ebd4e845
hamzacanaydinli <hamzacanaydinli@gmail.com>
2025-01-14 00:45:39 +0300

    Sonn

---
+++ E-ShopMVC/Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_ShopMVC.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
